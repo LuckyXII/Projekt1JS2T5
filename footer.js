02f0c0939f0ef891de7626d02a8de71be5342c2f
Arman Raftari <raftari1@hotmail.com>
2017-02-28 14:22:55 +0100

    footer update

--- footer.js
+++ footer.js
@@ -1,6 +1,27 @@
 document.write('\
 <link href="Css/style.css" rel="stylesheet">\
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>\
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>\
 <div class="row">\
-    <div id="footer"><img id="footerLogo" src="resources/Logo.png"></div>\
+    <div class="col-md-12" id="footer">\
+        <div id="socMedia" class="col-md-4 col-md-offset-5  col-xs-offset-4">\
+           <a href="http://www.facebook.com/"> <i class="fa fa-facebook-square" style="font-size:32px"></i></a>\
+           <a href="http://www.twitter.com/"><i class="fa fa-twitter" style="font-size:32px"></i></a>\
+           <a href="http://www.instagram.com/"><i class="fa fa-instagram" style="font-size:32px"></i></a>\
+        </div>\
+        <div class="col-md-6 col-md-offset-4 col-xs-offset-2" >\
+            <br>\
+            <a class="footerLinks" href="profil.html">Profil</a>\
+            <a class="footerLinks" href="aboutus.html">Om Oss</a>\
+            <a class="footerLinks" href="#">Kontakt</a>\
+            <a class="footerLinks" href="#">Faq</a>\
+            <a class="footerLinks" href="gallery.html">Gallery</a>\
+            <a class="footerLinks" href="#">Blogg</a>\
+        </div>\
+        <div class="col-md-4 col-md-offset-5 col-xs-offset-4 col-sm-offset-3">\
+            <img id="footerLogo" src="resources/Logo.png">\
+        </div>\
+    </div>\
 </div>\
 ');
